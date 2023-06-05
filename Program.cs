8d9edebb2826d9e759e902cdc4f56c12bde1e9e7
aluno <aluno@A2708>
2023-06-05 20:59:26 -0300

    Branch

--- Program.cs
+++ Program.cs
@@ -1,23 +1,23 @@
 ﻿using System;
 using System.Globalization;
 
-namespace Course 
-{ 
-    public class Program
+namespace Course
+{
+    public class While
     {
         public static void Main(string[] args)
         {
-            double num; //definindo a variavel
+            double num, i = 1; //definindo as variavels e o valor de i
 
-            Console.WriteLine("digite um numero positivo"); //mensagem para a interface do usuario
-            num = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); //solicitando o valor para o usuario
+            Console.WriteLine("digite um numero"); //enviando uma informação para a interface do usuario
+            num = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); //recevendo um valor do usuario
 
-            Console.WriteLine("Os primeiros " + num.ToString(CultureInfo.InvariantCulture) + " Numeros pares são: "); //mensagem para o usuario
-
-            for (int i = 2; i <= num * 2; i += 2)
-            { //definindo o valor de i; enquanto i for menor igual ao valor de num multiplicado por 2 some
-                Console.Write(i.ToString(CultureInfo.InvariantCulture) + ", "); //mostre o valor de i pro usuario
+            while (i <= num)
+            { //enquanto i for menor ou igual a num faça
+                Console.WriteLine(i + ", "); //mostre o valor para o usuario
+                i++; //adicione mais 1 no valor de i
             }
+
 
         }
     }
