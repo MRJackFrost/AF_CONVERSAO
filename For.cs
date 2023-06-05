63086f0a9c9d5adee803485dcb0552bf9e27c779
aluno <aluno@A2708>
2023-06-05 20:14:23 -0300

    Adicionar arquivos de projeto.

---
+++ For.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course 
+{ 
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double num; //definindo a variavel
+
+            Console.WriteLine("digite um numero positivo"); //mensagem para a interface do usuario
+            num = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); //solicitando o valor para o usuario
+
+            Console.WriteLine("Os primeiros " + num.ToString(CultureInfo.InvariantCulture) + " Numeros pares são: "); //mensagem para o usuario
+
+            for (int i = 2; i <= num * 2; i += 2)
+            { //definindo o valor de i; enquanto i for menor igual ao valor de num multiplicado por 2 some
+                Console.Write(i.ToString(CultureInfo.InvariantCulture) + ", "); //mostre o valor de i pro usuario
+            }
+
+        }
+    }
+}
