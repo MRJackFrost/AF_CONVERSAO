66c2869f1af983296f5775f114e4ced55dc7efc4
aluno <aluno@A2708>
2023-06-05 21:41:48 -0300

    Branch

--- Program.cs
+++ Program.cs
@@ -2,23 +2,48 @@
 using System.Globalization;
 
 namespace Course 
-{ 
+{
+   
     public class Program
     {
+
         public static void Main(string[] args)
         {
-            double num; //definindo a variavel
+            double soma = Soma(6, 4);
+            double mult = Mult(6, 4);
+            double sub = Sub(6, 4);
+            double div = Div(6, 4);
 
-            Console.WriteLine("digite um numero positivo"); //mensagem para a interface do usuario
-            num = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); //solicitando o valor para o usuario
+            Console.WriteLine(soma.ToString(CultureInfo.InvariantCulture));
+            Console.WriteLine(mult.ToString(CultureInfo.InvariantCulture));
+            Console.WriteLine(sub.ToString(CultureInfo.InvariantCulture));
+            Console.WriteLine(div.ToString(CultureInfo.InvariantCulture));
 
-            Console.WriteLine("Os primeiros " + num.ToString(CultureInfo.InvariantCulture) + " Numeros pares são: "); //mensagem para o usuario
 
-            for (int i = 2; i <= num * 2; i += 2)
-            { //definindo o valor de i; enquanto i for menor igual ao valor de num multiplicado por 2 some
-                Console.Write(i.ToString(CultureInfo.InvariantCulture) + ", "); //mostre o valor de i pro usuario
-            }
-
+        }
+        public static double Soma(double a, double b)
+        {
+            
+            double soma = a + b;
+            return soma;
+        }
+        public static double Mult(double a, double b)
+        {
+           
+            double mult = a * b;
+            return mult;
+        }
+        public static double Sub(double a, double b)
+        {
+            
+            double sub = a - b;
+            return sub;
+        }
+        public static double Div(double a, double b)
+        {
+            
+            double div = a / b;
+            return div;
         }
     }
 }
