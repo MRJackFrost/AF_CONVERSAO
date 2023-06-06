ff789daa6966d37efbac13bab9d8232b35eee902
aluno <aluno@A2708>
2023-06-05 22:01:16 -0300

    Branch

--- Program.cs
+++ Program.cs
@@ -9,41 +9,51 @@ namespace Course
 
         public static void Main(string[] args)
         {
-            double soma = Soma(6, 4);
-            double mult = Mult(6, 4);
-            double sub = Sub(6, 4);
-            double div = Div(6, 4);
+            char op; //definindo variavel
+            float num1, num2, calc; //definindo variavel
+            char per = 's'; //definindo variavel e o valor
 
-            Console.WriteLine(soma.ToString(CultureInfo.InvariantCulture));
-            Console.WriteLine(mult.ToString(CultureInfo.InvariantCulture));
-            Console.WriteLine(sub.ToString(CultureInfo.InvariantCulture));
-            Console.WriteLine(div.ToString(CultureInfo.InvariantCulture));
+            while (per == 's')
+            { //enquanto per for igual a s faça
+
+                Console.WriteLine("ENTRE COM O OPERADOR: +, -, /, *: ");//mensagem na interface do usuario
+                op = char.Parse(Console.ReadLine()); //solicitando o valor do char
+                Console.WriteLine("ENTRE COM OS VALORES"); //mensagem na interface do usuario
+                num1 = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                num2 = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                switch (op)
+                {
+                    case '+'://caso valor for +
+                        Console.Write(num1.ToString(CultureInfo.InvariantCulture) + " + " + num2.ToString(CultureInfo.InvariantCulture) + " = ");
+                        calc = num1 + num2;
+                        Console.WriteLine(calc.ToString(CultureInfo.InvariantCulture));
+                        break;//pare o switch
+                    case '-'://caso valor for -
+                        Console.Write(num1.ToString(CultureInfo.InvariantCulture) + " - " + num2.ToString(CultureInfo.InvariantCulture) + " = ");
+                        calc = num1 - num2;
+                        Console.WriteLine(calc.ToString(CultureInfo.InvariantCulture));
+                        break;//pare o switch
+                    case '/'://caso valor for /
+                        Console.Write(num1.ToString(CultureInfo.InvariantCulture) + " / " + num2.ToString(CultureInfo.InvariantCulture) + " = ");
+                        calc = num1 / num2;
+                        Console.WriteLine(calc.ToString(CultureInfo.InvariantCulture));
+                        break;//pare o switch
+                    case '*'://caso valor for *
+                        Console.Write(num1.ToString(CultureInfo.InvariantCulture) + " * " + num2.ToString(CultureInfo.InvariantCulture) + " = ");
+                        calc = num1 * num2;
+                        Console.WriteLine(calc.ToString(CultureInfo.InvariantCulture));
+                        break;//pare o switch
+                    default://se não for nenhum dos valores necessarios
+                        Console.WriteLine("OPERADOR ERRADO, POR FAVOR INSIRA CORRETAMENTE");
+                        break;//pare o switch
+                }
+                Console.WriteLine("quer continuar com a calculadora? s/n"); //mensagem para o usuario
+                per = char.Parse(Console.ReadLine()); //pedindo o valor de per
+            }
+            Console.WriteLine("Calculadora Finalizada"); //mensagem para o usuario
 
 
-        }
-        public static double Soma(double a, double b)
-        {
-            
-            double soma = a + b;
-            return soma;
-        }
-        public static double Mult(double a, double b)
-        {
-           
-            double mult = a * b;
-            return mult;
-        }
-        public static double Sub(double a, double b)
-        {
-            
-            double sub = a - b;
-            return sub;
-        }
-        public static double Div(double a, double b)
-        {
-            
-            double div = a / b;
-            return div;
         }
     }
 }
